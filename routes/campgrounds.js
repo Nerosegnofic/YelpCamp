968201c67684ab85543ee32bef8deaea2fcf29f2
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2024-08-25 19:23:10 +0300

    Added More Authorization for Reviews
    Now, you can't delete a review unless you are the author of it.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -26,7 +26,12 @@ router.post("/", isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 router.get("/:id", catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id).populate("reviews").populate("author");
+    const campground = await Campground.findById(id).populate({
+        path: "reviews",
+        populate: {
+            path: "author"
+        }
+    }).populate("author");
     if (!campground) {
         req.flash("error", "Cannot find that campground!");
         return res.redirect("/campgrounds");
